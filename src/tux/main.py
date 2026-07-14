76d787835ce761e633e5ab7afb712d34e6d1588d
peanut <pnuthacking@gmail.com>
2026-07-14 22:46:20 +0200

    Update main.py

--- src/tux/main.py
+++ src/tux/main.py
@@ -4,7 +4,8 @@ import time
 import random
 from datetime import date
 
-SAVE_FILE = "save.json"
+HOME = os.path.expanduser("~")
+SAVE_FILE = os.path.join(HOME, ".tux_pet_save.json")
 
 default_data = {
     "coins": 0,
@@ -18,234 +19,241 @@ default_data = {
 def load():
     if os.path.exists(SAVE_FILE):
         with open(SAVE_FILE, "r") as f:
-            return json.load(f)
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return default_data.copy()
     return default_data.copy()
 
 def save(data):
     with open(SAVE_FILE, "w") as f:
         json.dump(data, f, indent=4)
 
-data = load()
+def run_game():
+    data = load()
 
-if data["uses"] == 0:
-    print("yo this is ur first time opening the game")
-    name = input("what do u wanna name ur penguin? ")
-    if name.strip() == "":
-        name = "penguin"
-    data["name"] = name
-data["uses"] += 1
-os.system("cls" if os.name == "nt" else "clear")
-
-today = str(date.today())
-if data["last_daily"] != today:
-    data["coins"] += 10
-    data["last_daily"] = today
-    print("daily reward claimed! (+10 coins)")
-
-print(f"welcome back, mr {data['name']}")
-while True:
-    time.sleep(2)
+    if data["uses"] == 0:
+        print("yo this is ur first time opening the game")
+        name = input("what do u wanna name ur penguin? ")
+        if name.strip() == "":
+            name = "penguin"
+        data["name"] = name
+    data["uses"] += 1
     os.system("cls" if os.name == "nt" else "clear")
-    if data["happiness"] <= 20:
-        print("   _   ")
-        print(" (╥_╥) ")
-        print("//-=-\\\\")
-        print("(\\_=_/)")
-        print(" ^^ ^^ ")
-    elif 21 <= data["happiness"] <= 79:
-        print("   _   ")
-        print(" ( - ) ")
-        print("//-=-\\\\")
-        print("(\\_=_/)")
-        print(" ^^ ^^ ")
-    else:
-        print("   _   ")
-        print(" ( v ) ")
-        print("//-=-\\\\")
-        print("(\\_=_/)")
-        print(" ^^ ^^ ")
 
-    print("\n=== TUX PET ===")
-    print("your pet:", data["name"])
-    print("coins:", data["coins"])
-    print("happiness:", data["happiness"])
-    print("times opened:", data["uses"])
-    print("level:", data["level"])
+    today = str(date.today())
+    if data["last_daily"] != today:
+        data["coins"] += 10
+        data["last_daily"] = today
+        print("daily reward claimed! (+10 coins)")
 
-    print("\nwhat do u wanna do?")
-    print("1. show status")
-    print("2. change name (cost 50 coins)")
-    print("3. add happiness (cost 10 coins)")
-    print("4. open a case (cost 25 coins)")
-    print("5. talk to the penguin")
-    print("6. shop")
-    print("7. exit")
-
-    choice = input("> ")
-
-    if choice == "1":
-        print("\n=== STATUS ===")
-        print(data)
-    elif choice == "2":
-        if data["coins"] >= 50:
-            name = input("new name: ")
-            if name.strip() != "":
-                data["name"] = name
-                data["coins"] -= 50
-                print("name changed!")
-        else:
-            print("not enough coins :(")
-    elif choice == "3":
-        if data["coins"] >= 10:
-            data["happiness"] = min(100, data["happiness"] + 10)
-            data["coins"] -= 10
-            print("your penguin is happier!")
-        else:
-            print("not enough coins")
-    elif choice == "4":
-        if data["coins"] >= 25:
-            data["coins"] -= 25
-            print("opening a case...")
-            time.sleep(1)
-            cas = random.randint(1, 100)
-            if cas == 100:
-                print("JACKPOT! You won 100 coins!")
-                data["coins"] += 100
-            elif cas >= 80:
-                print("You won 50 coins!")
-                data["coins"] += 50
-            elif cas >= 30:
-                print("You won 10 coins!")
-                data["coins"] += 10
-            else:
-                print("You didnt win anything!")
-        else:
-            print("not enough coins")
-    elif choice == "5":
-        print("1. Compliment the penguin")
-        print("2. Be mean")
-        print("3. Flirt")
-        print("4. Tell a joke")
-        print("5. Stare at penguin")
-        choi = input("Choose: ")
-        if choi == "1":
-            print("You tell penguin he is the best bird...\n")
-            time.sleep(2)
-            print("Penguin is happier now!")
-            print("   _   ")
-            print(" (·u·) ")
-            print("//-=-\\\\")
-            print("(\\_=_/)")
-            print(" ^^ ^^ ")
-            data["happiness"] = min(100, data["happiness"] + 5)
-        elif choi == "2":
-            print("You tell penguin he is too heavy to fly...\n")
-            time.sleep(2)
-            print("Why are you so mean to him?")
+    print(f"welcome back, mr {data['name']}")
+    while True:
+        time.sleep(2)
+        os.system("cls" if os.name == "nt" else "clear")
+        if data["happiness"] <= 20:
             print("   _   ")
             print(" (╥_╥) ")
             print("//-=-\\\\")
             print("(\\_=_/)")
             print(" ^^ ^^ ")
-            data["happiness"] = max(0, data["happiness"] - 15)
-        elif choi == "3":
-            print("You flirt with penguin...\n")
-            time.sleep(1)
-            print("You dont know the result...")
+        elif 21 <= data["happiness"] <= 79:
             print("   _   ")
-            print(" (~v~) ")
-            print("//-=-\\\\")
-            print("(\\_=_/)")
-            print(" ^^ ^^ ")
-            flr = random.randint(0, 1)
-            if flr == 0:
-                print("Penguin liked it!")
-                data["happiness"] = min(100, data["happiness"] + 20)
-            else:
-                print("Penguin got uncomfortable...")
-                data["happiness"] = max(0, data["happiness"] - 10)
-        elif choi == "4":
-            print("You tell penguin a joke...\n")
-            time.sleep(2)
-            joke = random.randint(0, 10)
-            if joke <= 9:
-                print("The joke was good and penguin laughed!")
-                print("   _   ")
-                print(".·°՞(˃ᗜ˂)՞°·.")
-                print("//-=-\\\\")
-                print("(\\_=_/)")
-                print(" ^^ ^^ ")
-                data["happiness"] = min(100, data["happiness"] + 10)
-                time.sleep(0.2)
-            else:
-                print("Couldn't you think of a better joke..?")
-                print("   _   ")
-                print(" ( - ) ")
-                print("//-=-\\\\")
-                print("(\\_=_/)")
-                print(" ^^ ^^ ")
-                data["happiness"] = max(0, data["happiness"] - 5)
-                time.sleep(0.4)
-        elif choi == "5":
-            print("You stared at penguin...\n")
-            time.sleep(3)
-            print("The penguin stared back!")
-            print("   _   ")
-            print(" (⏺_⏺) ")
+            print(" ( - ) ")
             print("//-=-\\\\")
             print("(\\_=_/)")
             print(" ^^ ^^ ")
         else:
-            print("Wrong answer!")
-            time.sleep(1)
-    elif choice == "6":
-        print("You enter the shop...")
-        time.sleep(2)
-        print("What would you like to buy?")
-        print("1. Fish (10 coins)")
-        print("2. Ice Cream (5 coins)")
-        print("3. Toy (15 coins)")
-        print("4. Level Up (100 coins)")
-        print("5. Exit Shop")
-        shop = input("Enter your choice: ")
-        if shop == "1":
-            if data["coins"] >= 10:
-                print("You bought some fish!")
-                data["coins"] -= 10
-                data["happiness"] = min(100, data["happiness"] + 5)
-            else:
-                print("Not enough coins!")
-        elif shop == "2":
-            if data["coins"] >= 5:
-                print("You bought some ice cream!")
-                data["coins"] -= 5
-                data["happiness"] = min(100, data["happiness"] + 2)
-            else:
-                print("Not enough coins!")
-        elif shop == "3":
-            if data["coins"] >= 15:
-                print("You bought a toy!")
-                data["coins"] -= 15
-                data["happiness"] = min(100, data["happiness"] + 10)
-            else:
-                print("Not enough coins!")
-        elif shop == "4":
-            if data["coins"] >= 100:
-                print("You leveled up!")
-                data["coins"] -= 100
-                data["level"] += 1
-            else:
-                print("Not enough coins to level up!")
-        elif shop == "5":
-            print("Exiting shop...")
-        else:
-            print("Invalid option!")
-    elif choice == "7" or choice.lower() == "exit" or choice == "0":
-        save(data)
-        print("bye")
-        break
-    else:
-        print("invalid option")
+            print("   _   ")
+            print(" ( v ) ")
+            print("//-=-\\\\")
+            print("(\\_=_/)")
+            print(" ^^ ^^ ")
 
-    data["happiness"] = max(0, min(100, data["happiness"]))
-    save(data)
+        print("\n=== TUX PET ===")
+        print("your pet:", data["name"])
+        print("coins:", data["coins"])
+        print("happiness:", data["happiness"])
+        print("times opened:", data["uses"])
+        print("level:", data["level"])
+
+        print("\nwhat do u wanna do?")
+        print("1. show status")
+        print("2. change name (cost 50 coins)")
+        print("3. add happiness (cost 10 coins)")
+        print("4. open a case (cost 25 coins)")
+        print("5. talk to the penguin")
+        print("6. shop")
+        print("7. exit")
+
+        choice = input("> ")
+
+        if choice == "1":
+            print("\n=== STATUS ===")
+            print(data)
+        elif choice == "2":
+            if data["coins"] >= 50:
+                name = input("new name: ")
+                if name.strip() != "":
+                    data["name"] = name
+                    data["coins"] -= 50
+                    print("name changed!")
+            else:
+                print("not enough coins :(")
+        elif choice == "3":
+            if data["coins"] >= 10:
+                data["happiness"] = min(100, data["happiness"] + 10)
+                data["coins"] -= 10
+                print("your penguin is happier!")
+            else:
+                print("not enough coins")
+        elif choice == "4":
+            if data["coins"] >= 25:
+                data["coins"] -= 25
+                print("opening a case...")
+                time.sleep(1)
+                cas = random.randint(1, 100)
+                if cas == 100:
+                    print("JACKPOT! You won 100 coins!")
+                    data["coins"] += 100
+                elif cas >= 80:
+                    print("You won 50 coins!")
+                    data["coins"] += 50
+                elif cas >= 30:
+                    print("You won 10 coins!")
+                    data["coins"] += 10
+                else:
+                    print("You didnt win anything!")
+            else:
+                print("not enough coins")
+        elif choice == "5":
+            print("1. Compliment the penguin")
+            print("2. Be mean")
+            print("3. Flirt")
+            print("4. Tell a joke")
+            print("5. Stare at penguin")
+            choi = input("Choose: ")
+            if choi == "1":
+                print("You tell penguin he is the best bird...\n")
+                time.sleep(2)
+                print("Penguin is happier now!")
+                print("   _   ")
+                print(" (·u·) ")
+                print("//-=-\\\\")
+                print("(\\_=_/)")
+                print(" ^^ ^^ ")
+                data["happiness"] = min(100, data["happiness"] + 5)
+            elif choi == "2":
+                print("You tell penguin he is too heavy to fly...\n")
+                time.sleep(2)
+                print("Why are you so mean to him?")
+                print("   _   ")
+                print(" (╥_╥) ")
+                print("//-=-\\\\")
+                print("(\\_=_/)")
+                print(" ^^ ^^ ")
+                data["happiness"] = max(0, data["happiness"] - 15)
+            elif choi == "3":
+                print("You flirt with penguin...\n")
+                time.sleep(1)
+                print("You dont know the result...")
+                print("   _   ")
+                print(" (~v~) ")
+                print("//-=-\\\\")
+                print("(\\_=_/)")
+                print(" ^^ ^^ ")
+                flr = random.randint(0, 1)
+                if flr == 0:
+                    print("Penguin liked it!")
+                    data["happiness"] = min(100, data["happiness"] + 20)
+                else:
+                    print("Penguin got uncomfortable...")
+                    data["happiness"] = max(0, data["happiness"] - 10)
+            elif choi == "4":
+                print("You tell penguin a joke...\n")
+                time.sleep(2)
+                joke = random.randint(0, 10)
+                if joke <= 9:
+                    print("The joke was good and penguin laughed!")
+                    print("   _   ")
+                    print(".·°՞(˃ᗜ˂)՞°·.")
+                    print("//-=-\\\\")
+                    print("(\\_=_/)")
+                    print(" ^^ ^^ ")
+                    data["happiness"] = min(100, data["happiness"] + 10)
+                    time.sleep(0.2)
+                else:
+                    print("Couldn't you think of a better joke..?")
+                    print("   _   ")
+                    print(" ( - ) ")
+                    print("//-=-\\\\")
+                    print("(\\_=_/)")
+                    print(" ^^ ^^ ")
+                    data["happiness"] = max(0, data["happiness"] - 5)
+                    time.sleep(0.4)
+            elif choi == "5":
+                print("You stared at penguin...\n")
+                time.sleep(3)
+                print("The penguin stared back!")
+                print("   _   ")
+                print(" (⏺_⏺) ")
+                print("//-=-\\\\")
+                print("(\\_=_/)")
+                print(" ^^ ^^ ")
+            else:
+                print("Wrong answer!")
+                time.sleep(1)
+        elif choice == "6":
+            print("You enter the shop...")
+            time.sleep(2)
+            print("What would you like to buy?")
+            print("1. Fish (10 coins)")
+            print("2. Ice Cream (5 coins)")
+            print("3. Toy (15 coins)")
+            print("4. Level Up (100 coins)")
+            print("5. Exit Shop")
+            shop = input("Enter your choice: ")
+            if shop == "1":
+                if data["coins"] >= 10:
+                    print("You bought some fish!")
+                    data["coins"] -= 10
+                    data["happiness"] = min(100, data["happiness"] + 5)
+                else:
+                    print("Not enough coins!")
+            elif shop == "2":
+                if data["coins"] >= 5:
+                    print("You bought some ice cream!")
+                    data["coins"] -= 5
+                    data["happiness"] = min(100, data["happiness"] + 2)
+                else:
+                    print("Not enough coins!")
+            elif shop == "3":
+                if data["coins"] >= 15:
+                    print("You bought a toy!")
+                    data["coins"] -= 15
+                    data["happiness"] = min(100, data["happiness"] + 10)
+                else:
+                    print("Not enough coins!")
+            elif shop == "4":
+                if data["coins"] >= 100:
+                    print("You leveled up!")
+                    data["coins"] -= 100
+                    data["level"] += 1
+                else:
+                    print("Not enough coins to level up!")
+            elif shop == "5":
+                print("Exiting shop...")
+            else:
+                print("Invalid option!")
+        elif choice == "7" or choice.lower() == "exit" or choice == "0":
+            save(data)
+            print("bye")
+            break
+        else:
+            print("invalid option")
+
+        data["happiness"] = max(0, min(100, data["happiness"]))
+        save(data)
+
+if __name__ == "__main__":
+    run_game()
